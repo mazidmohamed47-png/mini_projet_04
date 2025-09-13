001ff456768b2f822b0a7fc7303a9bf9afbe84c5
Mazid Mohammed <mazidmohamed47@gmail.com>
2025-09-13 16:29:37 +0100

    modifier mini_projet_04

--- mini_projet_04.c
+++ mini_projet_04.c
@@ -28,23 +28,26 @@ etudiants tab_etudiants[max_etudiants];
 int nombre_etudiants=0;
 
 cours tab_cours[max_cours];
-int nomber_cours=0;
+int nombre_cours=0;
 
 
 void Ajouter_des_etudiants(){
 
-int i;
+int i,n;
+int debut;
 
 do {
+        debut=nombre_etudiants;
     printf("veuillez entrer le nombre de etudiants : ");
-    scanf("%d",&nombre_etudiants);
-    if(nombre_etudiants > max_etudiants || nombre_etudiants <= 0)
+    scanf("%d",&n);
+
+    if( (nombre_etudiants+n) > max_etudiants || n<= 0)
     printf("Erreur ! Nombre invalide. Veuillez reessayer.\n");
 }
-while(nombre_etudiants > max_etudiants || nombre_etudiants<=0);
+while((nombre_etudiants+n) > max_etudiants || n<=0);
 
-for (i=0;i<nombre_etudiants;i++){
-    printf(" Etudiants : %d\n",i+1);
+for (i=debut;i<debut+n;i++){
+    printf("Etudiants : %d\n",i+1);
 
     printf("veuillez entrer ID de etudiant : ");
     scanf("%d",&tab_etudiants[i].ID);
@@ -62,19 +65,30 @@ for (i=0;i<nombre_etudiants;i++){
     scanf("%f",&tab_etudiants[i].moyenne);
 
    }
-};
+
+   nombre_etudiants+=n; //puor mesajeur le total
+
+}
 
 void Ajouter_des_cours (){
-    int i;
-    printf("veuillez entrer les nomber des cours : ");
-    scanf("%d",&nomber_cours);
 
-    if (nomber_cours>50)
+    int i,n;
+    int debut;
 
-        printf("veuillez entrer un nomber mois de 50");
-    else if (nomber_cours<=50){
-    for (i=0;i<nomber_cours;i++){
-        printf(" cours %d",i+1);
+do {
+    debut = nombre_cours;
+
+    printf("veuillez entrer le nombre des cours : ");
+    scanf("%d",&n);
+
+    if( (nombre_cours+n) > max_cours || n<= 0)
+    printf("Erreur ! Nombre invalide. Veuillez reessayer.\n");
+}
+while((nombre_cours+n) > max_cours || n<=0);
+
+    for (i=debut;i<debut+n;i++){
+
+        printf(" cours : %d\n",i+1);
 
         printf("veuillez entrer le code :  ");
         scanf("%d",&tab_cours[i].code);
@@ -89,40 +103,55 @@ void Ajouter_des_cours (){
         scanf("%f",&tab_cours[i].note);
 
     }
-  }
-};
+  nombre_cours+=n;
+}
 
 void Afficher_la_liste_des_etudiants(){
 
 int i;
 
-    printf("la liste des etudiants: \n");
+if (nombre_etudiants==0)
+
+    printf(" !! la liste des etudiants est vied !!\n");
+
+    else {
+
+    printf("la liste des etudiants : \n");
 
 for (i=0;i<nombre_etudiants;i++){
 
-    printf("\n=================================\n");
+    printf("\n==========================\n");
 
     printf("Etudiants : %d\n",i+1);
 
-    printf("ID : %d\n",tab_etudiants[i].ID);
+    printf("ID        : %d\n",tab_etudiants[i].ID);
 
-    printf("nom : %s\n",tab_etudiants[i].nom);
+    printf("nom       : %s\n",tab_etudiants[i].nom);
 
-    printf("prenom : %s \n",tab_etudiants[i].prenom);
+    printf("prenom    : %s \n",tab_etudiants[i].prenom);
 
-    printf("Age : %d \n",tab_etudiants[i].age);
+    printf("Age       : %d \n",tab_etudiants[i].age);
 
-    printf("moyenne : %f\n",tab_etudiants[i].moyenne);
+    printf("moyenne   : %.2f\n",tab_etudiants[i].moyenne);
 
 }
 
-};
-void Afficher_la_liste_des_cours(){
-int i;
+    }
 
-printf("la liste des cours : ");
-if (nomber_cours<=50){
-for (i=0;i<nomber_cours;i++){
+}
+void Afficher_la_liste_des_cours(){
+  int i;
+
+  if (nombre_cours==0){
+
+    printf(" !! la liste des etudiants est vied !!\n");
+  }
+    else {
+
+    printf("==========================\n");
+
+    printf("la liste des cours : \n");
+  for (i=0;i<nombre_cours;i++){
 
     printf(" coures : %d \n",i+1);
 
@@ -132,45 +161,31 @@ for (i=0;i<nomber_cours;i++){
 
     printf("credits : %d \n",tab_cours[i].credits);
 
-    printf("note : %f \n",tab_cours[i].note);
+    printf("note : %.2f \n",tab_cours[i].note);
+
+  }
+}
 
 }
-}
-else if (nomber_cours>50)
-
-    printf("le snomber des cour est tre gant veuiller entrer les nomber des cour");
-
-    else
-        printf(" !!Erreur!! le nomber tre petit  ");
-
-};
-
-
-
-
-
-
-
-
 
 int main()
 {
     int choix;
  do {
-    printf("==========Menu==========\n");
+    printf("============Menu==========\n");
 
-    printf("Ajouter des etudiants : 1\n");
+    printf("Ajouter des etudiants  : 1\n");
 
-    printf("Ajouter des cours : 2\n");
+    printf("Ajouter des cours      : 2\n");
 
     printf("Afficher les etudiants : 3\n");
 
-    printf(" Afficher les cours : 4\n");
+    printf("Afficher les cours     : 4\n");
 
-    printf("Quitter : 5 \n ");
+    printf("Quitter                : 5 \n");
 
-     printf("Votre choix : ");
-     scanf("%d", &choix);
+    printf("Votre choix            : ");
+    scanf("%d", &choix);
 
  switch (choix){
 
@@ -194,7 +209,9 @@ int main()
     Afficher_la_liste_des_cours();
     break;
 
-  case 5 : printf(" Au revoir ! \n");
+  case 5 :
+    printf("===========================\n");
+    printf("\tAu revoir ! \n");
     break;
 
   default  : printf("Choix invalide!\n");
